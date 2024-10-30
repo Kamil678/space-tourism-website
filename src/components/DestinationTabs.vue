cd2c5899d8061d9dd3fece51a1bcdcaf82d461d5
Kamil678 <kamilpigulak@interia.pl>
2024-10-30 10:36:15 +0100

    feat: refactor

--- src/components/DestinationTabs.vue
+++ src/components/DestinationTabs.vue
@@ -64,7 +64,7 @@ const tab = ref("moon");
 const destinations = [
   {
     name: "moon",
-    img: "src/assets/destination/image-moon.png",
+    img: "src/assets/destination/image-moon.webp",
     description:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites",
     distance: "384,400 km",
@@ -72,7 +72,7 @@ const destinations = [
   },
   {
     name: "mars",
-    img: "src/assets/destination/image-mars.png",
+    img: "src/assets/destination/image-mars.webp",
     description:
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
     distance: "225 mil. km",
@@ -80,7 +80,7 @@ const destinations = [
   },
   {
     name: "europa",
-    img: "src/assets/destination/image-europa.png",
+    img: "src/assets/destination/image-europa.webp",
     description:
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
     distance: "628 mil. km",
@@ -88,7 +88,7 @@ const destinations = [
   },
   {
     name: "titan",
-    img: "src/assets/destination/image-titan.png",
+    img: "src/assets/destination/image-titan.webp",
     description:
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
     distance: "1.6 bil. km",
